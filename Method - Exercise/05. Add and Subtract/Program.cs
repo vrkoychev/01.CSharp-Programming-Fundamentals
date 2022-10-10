0095619836b0bc2526c466801f2fbfa1e635f296
Valentin Koychev <114136127+vrkoychev@users.noreply.github.com>
2022-10-10 13:19:58 +0300

    Add files via upload

---
+++ Method - Exercise/05. Add and Subtract/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _05._Add_and_Subtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int returnedSumOfTheFirstTwo = GetSumOfTheFirstTwo(a, b);
+            int returnedSubtraction = GetTheSubtractionWithTheThird(returnedSumOfTheFirstTwo, c);
+
+            Console.WriteLine(returnedSubtraction);
+        }
+
+        private static int GetTheSubtractionWithTheThird(int returnedSumOfTheFirstTwo, int c)
+        {
+            return returnedSumOfTheFirstTwo - c;
+        }
+
+        private static int GetSumOfTheFirstTwo(int a, int b)
+        {
+            return a + b;
+        }
+
+
+    }
+}
