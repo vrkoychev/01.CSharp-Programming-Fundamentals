8c2402da16770ab35ac03f5fd92f6bb436f28c45
Valentin Koychev <114136127+vrkoychev@users.noreply.github.com>
2022-10-12 21:39:33 +0300

    Guess A Number By Valentin
    That's my own twist to the simple game Guess A Number!

---
+++ GueesANumber/GuessANumber.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace GueesANumber
+{
+    class GuessANumber
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+            int computerNumber = randomNumber.Next(1, 101);
+
+            while (true)
+            {
+                Console.Write("Guess a number (1-100): ");
+
+                string playerInput = Console.ReadLine();
+                bool isValid = int.TryParse(playerInput, out int playerNumber);
+
+                if (isValid)
+                {
+                    if (playerNumber == computerNumber)
+                    {
+                        Console.WriteLine("You guessed it!");
+                        break;
+                    }
+                    else if (playerNumber < computerNumber)
+                    {
+                        Console.WriteLine("Too Low");
+                    }
+                    else if (playerNumber > computerNumber)
+                    {
+                        Console.WriteLine("Too High");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
+                }
+            }
+
+            bool done = false;
+            while (!done)
+            {
+                Console.WriteLine("Do you want to play again?");
+                Console.Write("Yes/No: ");
+
+                string answer = Console.ReadLine();
+
+                if (answer == "Yes")
+                {
+                    randomNumber = new Random();
+                    computerNumber = randomNumber.Next(1, 101);
+
+                    while (true)
+                    {
+                        Console.Write("Guess a number (1-100): ");
+
+                        string playerInput = Console.ReadLine();
+                        bool isValid = int.TryParse(playerInput, out int playerNumber);
+
+                        if (isValid)
+                        {
+                            if (playerNumber == computerNumber)
+                            {
+                                Console.WriteLine("You guessed it!");
+                                break;
+                            }
+                            else if (playerNumber < computerNumber)
+                            {
+                                Console.WriteLine("Too Low");
+                            }
+                            else if (playerNumber > computerNumber)
+                            {
+                                Console.WriteLine("Too High");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input.");
+                        }
+                    }
+                }
+                else if (answer == "No")
+                {
+                    done = true;
+                }
+            }
+
+            Console.WriteLine("Sorry to see you go.");
+        }
+    }
+}
